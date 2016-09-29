4a668da4e5ae619924bc7d578cfef7683c383333
mpm04 <mpm04@MichaelsLaptop>
2016-09-29 15:12:10 -0400

    update 2 on 9/29/16

--- Lame RPG/src/src/Assassin.java
+++ Lame RPG/src/src/Assassin.java
@@ -5,11 +5,11 @@ public class Assassin extends Player
 
 	public Assassin(String initName)
 	{
-		super(100, 75, 45, 50, 10, 3, 5, initName);
+		super(100, 75, 45, 80, 10, 3, 5, initName);
 		HP = 100;
 		mana = 75;
 		damage = 45;
-		attack = 50;
+		attack = 80;
 		defense = 10;
 		character_class = 3;
 		magicDefense = 5;		
