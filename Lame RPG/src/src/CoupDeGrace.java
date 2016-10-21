af425015ce62102b860c13a6de099043b6325f2b
mpm04 <mpm04@MichaelsLaptop>
2016-10-21 12:32:31 -0400

    update on 10/21/16

---
+++ Lame RPG/src/src/CoupDeGrace.java
@@ -0,0 +1,27 @@
+package src;
+
+public class CoupDeGrace extends Spell
+{
+
+	public CoupDeGrace(Character c)
+	{
+		//TODO
+		super(tempname, dsc, manacost, level_rq, cd, c, targetable);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void cast()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void cast(Character target)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Lame RPG/src/src/ManaBreak.java
@@ -0,0 +1,27 @@
+package src;
+
+public class ManaBreak extends Spell
+{
+
+	public ManaBreak(Character c)
+	{
+		//TODO
+		super(tempname, dsc, manacost, level_rq, cd, c, targetable);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void cast()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void cast(Character target)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
