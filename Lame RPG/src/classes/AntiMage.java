c1f9ac277532067620733be2d433653203fe18fe
mpaul418 <mpm0418@gmail.com>
2017-01-10 12:21:42 -0500

    Update 2 on 1/10/17
    -Removed some comments in Anti-Mage class

--- Lame RPG/src/classes/AntiMage.java
+++ Lame RPG/src/classes/AntiMage.java
@@ -13,41 +13,19 @@ public class AntiMage extends Player
 	public AntiMage(String initName)
 	{
 		super(150, 60, 30, 75, 15, 1, 0.0, initName);
-		//HP = 150;
-		//mana = 60;
-		//damage = 30;
-		//attack = 75;
-		//defense = 15;
-		//character_class = 1;
-		//magicDefense = 10;
 		mana_break = new ManaBreak(this);
 		blink = new Blink(this);
 		spell_shield = new SpellShield(this);
 		mana_void = new ManaVoid(this);
-		//mana_break.addToSpellbook(this);//TODO
-		//blink.addToSpellbook(this);
-		//spell_shield.addToSpellbook(this);
-		//unlearned_spells.add(mana_void);
 	}
 	
 	public AntiMage(int nHP, int nMana, int nDmg, int nAcc, int nDfs, double nMgDef, String initName)
 	{
 		super(nHP, nMana, nDmg, nAcc, nDfs, 1, nMgDef, initName);
-		//HP = nHP;
-		//mana = nMana;
-		//damage = 30;
-		//attack = 75;
-		//defense = 15;
-		//character_class = 1;
-		//magicDefense = 10;
 		mana_break = new ManaBreak(this);
 		blink = new Blink(this);
 		spell_shield = new SpellShield(this);
 		mana_void = new ManaVoid(this);
-		//mana_break.addToSpellbook(this);
-		//blink.addToSpellbook(this);
-		//spell_shield.addToSpellbook(this);
-		//unlearned_spells.add(mana_void);
 	}
 	
 	@Override
